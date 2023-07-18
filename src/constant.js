6d4beb55a5aeda99284faf1f2e91a4db03632bca
jaswantjs <jaswant@ryaz.io>
2023-07-18 22:04:25 +0530

    Fix: cdn issue

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,6 @@
 export const constants = () => {
 return ({
     API_BASE_URL: "https://hammerhead-app-9bqxa.ondigitalocean.app/",
+    DO_BUCKET_URL: "https://alumni-gndec.nyc3.cdn.digitaloceanspaces.com/Alumni/"
 })
 }
