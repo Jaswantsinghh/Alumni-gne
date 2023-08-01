d48ceff289b46e4c54dfb3a98781052110669b25
jassBawa <jasspreetsbawa@gmail.com>
2023-08-02 00:03:54 +0530

    feat: about, contact page updated

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,134 +4,96 @@ function AboutPage() {
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
-        <h1 className="sm:text-3xl text-lg font-medium text-center text-gray-900 mb-20">
-          About Page is under construction
-        </h1>
-        {/* <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+        <div className="flex text-justify flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
           <div className="p-4 md:w-1/3 flex">
             <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
               <svg
                 fill="none"
                 stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-6 h-6"
-                viewBox="0 0 24 24"
-              >
-                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-              </svg>
-            </div>
-            <div className="flex-grow pl-6">
-              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-                Challenges of Event Planning
-              </h2>
-              <p className="leading-relaxed text-base">
-                At Eventify, we understand that planning and executing events
-                can be a daunting task. Whether it's a small business conference
-                or a large-scale music festival, there are countless details to
-                consider, timelines to manage, and stakeholders to communicate
-                with.
-              </p>
-              <a className="mt-3 text-indigo-500 inline-flex items-center">
-                Learn More
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 ml-2"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div className="p-4 md:w-1/3 flex">
-            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-6 h-6"
-                viewBox="0 0 24 24"
-              >
-                <circle cx="6" cy="6" r="3"></circle>
-                <circle cx="6" cy="18" r="3"></circle>
-                <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-              </svg>
-            </div>
-            <div className="flex-grow pl-6">
-              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-                Our Solution
-              </h2>
-              <p className="leading-relaxed text-base">
-                That's why we've created a comprehensive project management
-                system that streamlines the entire process, making it easier and
-                more efficient for you to bring your event to life
-              </p>
-              <a className="mt-3 text-indigo-500 inline-flex items-center">
-                Learn More
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 ml-2"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div className="p-4 md:w-1/3 flex">
-            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-6 h-6"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-6 h-6"
                 viewBox="0 0 24 24"
               >
                 <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                 <circle cx="12" cy="7" r="4"></circle>
               </svg>
             </div>
-            <div className="flex-grow pl-6">
+            <div className="flex-grow pl-4">
               <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-                Our Team
+                GNDEC Alumni: Uniting Success
               </h2>
               <p className="leading-relaxed text-base">
-                Our team of experienced event planners and software developers
-                have worked together to create a user-friendly platform that
-                empowers event organizers to plan, organize, and execute their
-                events with confidence.
+                We are the Alumni of Guru Nanak Dev Engineering College,
+                Ludhiana India. The engineers produced by GNDEC are successfully
+                employed in India & many countries all over the world making
+                significant contributions to the modern economy. This website is
+                the start of an effort to re-unite and recognize the GNDEC
+                Alumni from India and other nations, where they reside.
               </p>
-              <a className="mt-3 text-indigo-500 inline-flex items-center">
-                Learn More
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 ml-2"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
             </div>
           </div>
-        </div> */}
+          <div className="p-4 md:w-1/3 flex">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
+              </svg>
+            </div>
+            <div className="flex-grow pl-4">
+              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+                Nurturing Engineers since 1956
+              </h2>
+              <p className="leading-relaxed text-base">
+                Guru Nanak Dev Engineering College was established in 1956 by
+                the Nankana Sahib Education Trust, honoring the birthplace of
+                Guru Nanak Dev Ji, with a motto to uplift the vast rural
+                villages of Punjab and rest of India. The college set aside
+                majority of admissions every year for the students from rural
+                areas. The college foundation stone was laid by Late Dr.
+                Rajendra Prasad Ji, the First President of India. Nearly 10,000
+                graduate and 3000 Post Graduate Engineers have graduated from
+                this college since its formation and this tradition continues
+                successfully.
+              </p>
+            </div>
+          </div>
+          <div className="p-4 md:w-1/3 flex">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+              </svg>
+            </div>
+            <div className="flex-grow pl-4">
+              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+                Who made this website?
+              </h2>
+              <p className="leading-relaxed text-base">
+                In 2023, the graduating alumni batch from 1973 made this effort
+                to develop a GENCO Alumni Association website to memorialize all
+                the past students in one place and connect them to a common
+                database for reference and collective memory of this great
+                institution.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
